1002de1aa06e753e011ecbcff1278a0e88015f37
adamsjer000 <adamsjer000@warren.k12.in.us>
2022-02-09 12:13:44 -0500

    important

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //Create a new car with Make, Model, and Year Constructor
-        Car car1 = new Car("Chevy", "Camaro", 1977);
-        //Make a new car with Default Constructor
-        Car car2 = new Car();
 
-        System.out.println(car1);
-        System.out.println(car2);
+        Rect rect1 = new Rect(2,4);
+
+        System.out.println(rect1.getLength());
+        System.out.println(rect1.getWidth());
 
 
+        rect1.setWidth(5);
+        rect1.setLength(10);
+        System.out.println(rect1.getLength());
+        System.out.println(rect1.getWidth());
     }
 }
